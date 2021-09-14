9a7dad8f280d304688d8dd0f4fe696effdebb786
atiQkhalid <39992488+atiQkhalid@users.noreply.github.com>
2021-09-15 01:04:59 +0200

    permissions added crash fixed

--- app/src/main/java/com/example/appcentassignment/views/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/appcentassignment/views/fragment/HomeFragment.kt
@@ -1,27 +1,34 @@
 package com.example.appcentassignment.views.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.appcentassignment.base.BaseFragment
 import com.example.appcentassignment.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
+class HomeFragment : BaseFragment() {
 
     private lateinit var binding: FragmentHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
-        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            tv.text = "HomeFragment"
+        }
     }
 }
